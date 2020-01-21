a6b110551b29a48e9cbdb6eee3c99998286bbbe4
earthmant <trammell@cloudify.co>
2020-01-21 11:51:57 +0200

    some logging

--- .cicd/test_manager.py
+++ .cicd/test_manager.py
@@ -58,6 +58,11 @@ class OpenstackPluginTestCase(PluginsTest):
         self._create_secrets(secrets)
 
     def upload_plugins(self):
+        self.logger.info('In upload plugins.')
+        self.logger.info('This is the current dir: {0}'.format(os.curdir))
+        self.logger.info('The build dir is: {0}'.format(os.path.join(DEVELOPMENT_ROOT, PLUGIN_NAME)))
+        self.logger.info('The build dir is: {0}'.format(self.plugin_root_directory))
+        self.logger.info('This is what is in that directory: {0}'.format(os.listdir(self.plugin_root_directory)))
         self.upload_mock_plugin(
             PLUGIN_NAME,
             os.path.join(DEVELOPMENT_ROOT, PLUGIN_NAME))
